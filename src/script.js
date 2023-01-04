056bfff44f14ea84eb48ca5b3f8b81f84d6f8ee2
cplan85 <carlos_planchart@live.com>
2023-01-04 18:04:20 +0100

    Finished creating mesh group

--- src/script.js
+++ src/script.js
@@ -11,26 +11,36 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-//y goes up and down
-//x axis goes right or left
-//z going forward but it is default
-// mesh.position.x = 0.7
-// mesh.position.y = - 0.6
-// mesh.position.z = 1
 
-//another way to set x, y, and z value
-mesh.position.set(0.7, -0.6, 1)
+const group = new THREE.Group()
+scene.add(group)
 
-//mesh.scale.x = 2
-//mesh.scale.y = 0.5
-//mesh.scale.z = 0.5
-mesh.scale.set(2, 0.5, 0.5)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+     )
 
 
+group.add(cube1)
+group.position.y =1
+group.scale.y =2
+group.rotation.y = 1
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00})
+     )  
+     
+cube2.position.x = - 2;
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x00ff})
+     )  
+     
+cube3.position.x = 2;
+group.add(cube3)
 // Axes helper
 const axesHelper = new THREE.AxesHelper(2)
 scene.add(axesHelper);
@@ -39,7 +49,7 @@ scene.add(axesHelper);
 //mesh.position.normalize()
 
 //distance between the center of the scene and the object's position.
-console.log(mesh.position.length())
+//console.log(mesh.position.length())
 
 /**
  * Sizes
@@ -57,8 +67,10 @@ camera.position.z = 3
 
 scene.add(camera)
 
+//camera.lookAt(mesh.position)
+
 //distance between the center of the scene and the object's position.
-console.log(mesh.position.distanceTo(camera.position))
+//console.log(mesh.position.distanceTo(camera.position))
 
 /**
  * Renderer
@@ -68,3 +80,6 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+
+
